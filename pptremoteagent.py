001c674c9b3c23bb45049e70593c84ceecb1364b
mpapazog <spamandviruses@gmail.com>
2018-10-25 22:55:12 +0300

    Add files via upload

--- pptremoteagent.py
+++ pptremoteagent.py
@@ -5,7 +5,7 @@
 #  * Open the pptremoteserver's IP address with your smartphone's browser and control the slideshow
 #
 # Notes:
-#  * By default pptremoteagent.py polls localhost:5000. To have it poll a different address, run:
+#  * By default pptremoteagent.py polls localhost:8080. To have it poll a different address, run:
 #       pptremoteagent.py -s <serverip>:<serverport>
 #  * You will need to install the following Python 3 modules:
 #       On server: flask
