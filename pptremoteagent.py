b162ae2592598c28a7f787f711df8e136af35990
mpapazog <spamandviruses@gmail.com>
2018-10-25 22:43:01 +0300

    Add files via upload

--- pptremoteagent.py
+++ pptremoteagent.py
@@ -1,3 +1,16 @@
+# This is a script to remote control PowerPoint presentations on Windows from your smartphone. 
+# Usage:
+#  * Run pptremoteserver.py on a server accessible from the internet
+#  * Run pptremoteagent.py on the computer where you have PowerPoint running
+#  * Open the pptremoteserver's IP address with your smartphone's browser and control the slideshow
+#
+# Notes:
+#  * By default pptremoteagent.py polls localhost:5000. To have it poll a different address, run:
+#       pptremoteagent.py -s <serverip>:<serverport>
+#  * You will need to install the following Python 3 modules:
+#       On server: flask
+#       On agent: keyboard, pywin32, requests
+
 from win32gui import GetWindowText, GetForegroundWindow
 import time, requests, sys, getopt, keyboard
 
